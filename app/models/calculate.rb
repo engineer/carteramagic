88bc860614d837fc558f6b9ed6159cff802ff978
Ian <carian2996@gmail.com>
2014-07-23 06:01:27 +0000

    Prueba 3 con fallo a la hora de no poner meta

--- app/models/calculate.rb
+++ app/models/calculate.rb
@@ -5,15 +5,22 @@ class Calculate
   end
   
   def can_spend(amount_spend, day_spend)
-    
-    goal = @user.goals.where(["? >= goals.start_at and ? <= goals.end_at", day_spend, day_spend]).first
-    
+
+    goal = @user.goals.where(["? >= goals.start_at and ? <= goals.end_at", day_spend, day_spend])
+
     if goal.nil?
+      
       begin_date = day_spend.beginning_of_month.beginning_of_day
       end_date = day_spend.end_of_month.end_of_day
+
     else
-      begin_date = goal.start_at.beginning_of_day.beginning_of_day
-      end_date = goal.end_at.end_of_day.end_of_day
+
+      date_min = goal.map { |x| x.start_at }.min
+      date_max = goal.map { |x| x.end_at }.max
+
+      begin_date = date_min.beginning_of_month.beginning_of_day
+      end_date = date_max.end_of_month.end_of_day
+
     end
 
     incomes = @user.variable_payments.where(["variable_payments.income = ? AND variable_payments.created_at >= ? AND variable_payments.created_at <= ? ", true, begin_date, end_date]).sum(:amount)
@@ -23,11 +30,16 @@ class Calculate
     available = available - amount_spend
     
     if goal.nil?
+      
       result = available > 0
+
     else
-      result = available >= goal.amount
+      
+      sum_amount = goal.map { |x| x.amount }.inject{ |sum, x| sum + x }
+      result = available > sum_amount
+
     end
-    
+
     return result
     
   end
--- test/models/calculate_test.rb
+++ test/models/calculate_test.rb
@@ -2,53 +2,60 @@ require_relative '../test_helper'
 
 class CalculateTest < ActiveSupport::TestCase
   
-  test "give a nice answer for two goals" do
+#     test "give a nice answer for two (or more) goals" do
     
-    neto = User.new
-    neto.email = "neto@example.com"
-    neto.password = neto.password_confirmation = "12345678"
-    neto.save!
+#     # Create a new user
+#     neto = User.new
+#     neto.email = "neto@example.com"
+#     neto.password = neto.password_confirmation = "12345678"
+#     neto.save!
 
-    chelas = Goal.new
-    chelas.user = neto
-    chelas.amount = 300
-    chelas.start_at = Date.new(2014, 7, 13)
-    chelas.end_at = Date.new(2014, 7, 28)
-    chelas.save!
+#     # Create more than one goal
+#     chelas = Goal.new
+#     chelas.user = neto
+#     chelas.amount = 300
+#     chelas.start_at = Date.new(2014, 7, 13)
+#     chelas.end_at = Date.new(2014, 7, 28)
+#     chelas.save!
 
-    botana = Goal.new
-    botana.user = neto
-    botana.amount = 100
-    botana.start_at = Date.new(2014, 7, 10)
-    botana.end_at = Date.new(2014, 7, 24)
-    botana.save!
+#     botana = Goal.new
+#     botana.user = neto
+#     botana.amount = 100
+#     botana.start_at = Date.new(2014, 7, 10)
+#     botana.end_at = Date.new(2014, 7, 24)
+#     botana.save!
 
-    sueldo = VariablePayment.new
-    sueldo.user = neto
-    sueldo.amount = 1000
-    sueldo.income = true
-    sueldo.created_at = Date.new(2014, 7, 15)
-    sueldo.save!
+#     # Create new payments (incomes and outcomes)
+#     sueldo = VariablePayment.new
+#     sueldo.user = neto
+#     sueldo.amount = 1000
+#     sueldo.income = true # Income
+#     sueldo.created_at = Date.new(2014, 7, 15)
+#     sueldo.save!
 
-    nexus = VariablePayment.new
-    nexus.user = neto
-    nexus.amount = 400
-    nexus.income = false
-    nexus.created_at = Date.new(2014, 7, 20)
-    nexus.save!
+#     nexus = VariablePayment.new
+#     nexus.user = neto
+#     nexus.amount = 400
+#     nexus.income = false # Outcome
+#     nexus.created_at = Date.new(2014, 7, 20)
+#     nexus.save!
     
-    cena = VariablePayment.new
-    cena.user = neto
-    cena.amount = 100
-    cena.income = false
-    cena.created_at = Date.new(2014, 7, 21)
-    cena.save!    
+#     cena = VariablePayment.new
+#     cena.user = neto
+#     cena.amount = 100
+#     cena.income = false # Outcome
+#     cena.created_at = Date.new(2014, 7, 21)
+#     cena.save!
 
-  end
+#     # Can I spend 50 bucks in beer?
+#     decision = Calculate.new(neto)
+#     assert decision.can_spend(50, Date.today)
 
-
+#   end
 
   test "drop a good answer without any goal" do
+
+    # Verificar en linea 9
     
     gabs = User.new
     gabs.email = "gabs@example.com"
@@ -78,59 +85,60 @@ class CalculateTest < ActiveSupport::TestCase
     
     decision = Calculate.new(gabs)
     assert decision.can_spend(10, Date.today)
+    assert !decision.can_spend(30, Date.today)
     
     end 
    
-  test "can I spend some amount wtihout affect my goal?" do
+  # test "can I spend some amount wtihout affect my goal?" do
     
-    # Creacion de un usuario
-    ian = User.new
-    ian.email = "ian@example.com"
-    ian.password = ian.password_confirmation = "12345678"
-    ian.save! # Guardar en el servidor
+  #   # Creacion de un usuario
+  #   ian = User.new
+  #   ian.email = "ian@example.com"
+  #   ian.password = ian.password_confirmation = "12345678"
+  #   ian.save! # Guardar en el servidor
     
-    # Ingresos/egresos variables 
-    sueldo = VariablePayment.new
-    sueldo.user = ian
-    sueldo.amount = 200
-    sueldo.income = true
-    sueldo.created_at = 2.days.ago
-    sueldo.save!
+  #   # Ingresos/egresos variables 
+  #   sueldo = VariablePayment.new
+  #   sueldo.user = ian
+  #   sueldo.amount = 200
+  #   sueldo.income = true
+  #   sueldo.created_at = 2.days.ago
+  #   sueldo.save!
     
-    papas = VariablePayment.new
-    papas.user = ian
-    papas.amount = 10
-    papas.income = false
-    papas.created_at = 2.days.ago
-    papas.save!
+  #   papas = VariablePayment.new
+  #   papas.user = ian
+  #   papas.amount = 10
+  #   papas.income = false
+  #   papas.created_at = 2.days.ago
+  #   papas.save!
     
-    frutsi = VariablePayment.new
-    frutsi.user = ian
-    frutsi.amount = 5
-    frutsi.income = false
-    frutsi.created_at = 1.days.ago
-    frutsi.save!
+  #   frutsi = VariablePayment.new
+  #   frutsi.user = ian
+  #   frutsi.amount = 5
+  #   frutsi.income = false
+  #   frutsi.created_at = 1.days.ago
+  #   frutsi.save!
     
-    coca = VariablePayment.new
-    coca.user = ian
-    coca.amount = 15
-    coca.income = false
-    coca.created_at = Date.today
-    coca.save!
+  #   coca = VariablePayment.new
+  #   coca.user = ian
+  #   coca.amount = 15
+  #   coca.income = false
+  #   coca.created_at = Date.today
+  #   coca.save!
     
-    # Creacion de una meta
-    tacos = Goal.new
-    tacos.user = ian
-    tacos.amount = 100
-    tacos.start_at = 5.days.ago
-    tacos.end_at = Date.today
-    tacos.save!
+  #   # Creacion de una meta
+  #   tacos = Goal.new
+  #   tacos.user = ian
+  #   tacos.amount = 100
+  #   tacos.start_at = 5.days.ago
+  #   tacos.end_at = Date.today
+  #   tacos.save!
     
-    # Proceso para determinar si se puede gastar o no
-    calculadora = Calculate.new(ian)
-    assert calculadora.can_spend(50, Date.today) 
-    assert !calculadora.can_spend(300, Date.today) 
+  #   # Proceso para determinar si se puede gastar o no
+  #   decision = Calculate.new(ian)
+  #   assert decision.can_spend(50, Date.today) 
+  #   assert !decision.can_spend(300, Date.today) 
     
-   end
+  #  end
   
 end
