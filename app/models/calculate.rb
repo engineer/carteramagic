a92965a3e5a30afd569f3c15f3f743f00d28a8b0
Ian <carian2996@gmail.com>
2014-07-29 23:45:59 +0000

    Modified model for daily goal

--- app/models/calculate.rb
+++ app/models/calculate.rb
@@ -1,15 +1,15 @@
 class Calculate
-  
+
   def initialize(user)
     @user = user
   end
-  
+
   def can_spend(amount_spend, day_spend)
 
     goal = @user.goals.where(["? >= goals.start_at and ? <= goals.end_at", day_spend, day_spend])
 
     if goal.empty?
-      
+
       global_begin_date = day_spend.beginning_of_month.beginning_of_day
       global_end_date = day_spend.end_of_month.end_of_day
 
@@ -39,22 +39,22 @@ class Calculate
 
     term_until_now = day_spend.day - global_begin_date.day
     available_until_now = (available/term_until_now).round(3)
-    
+
     if goal.empty?
-      
+
       result = available > 0
-      
+
     else
 
       result = available_until_now > goal_per_day
-      
+
       # p available_until_now
       # p goal_per_day
 
     end
 
     return result
-    
+
   end
-  
+
 end
