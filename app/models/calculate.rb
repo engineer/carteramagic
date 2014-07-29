efe610f87e1555c7d4655239a746d610585bfd5f
Ian <carian2996@gmail.com>
2014-07-29 01:24:37 +0000

    Modified test to decide with goal_per_day

--- app/models/calculate.rb
+++ app/models/calculate.rb
@@ -15,49 +15,41 @@ class Calculate
 
     else
 
-      min_goal_date = goal.map { |x| x.start_at }.min.beginning_of_day
-      max_goal_date = goal.map { |x| x.end_at }.max.end_of_day
+      global_begin_date = goal.map { |x| x.start_at }.min
+      global_end_date = day_spend
 
-      #global_begin_date = min_goal_date.beginning_of_month.beginning_of_day
-      #global_end_date = max_goal_date.end_of_month.end_of_day
-
-      term = (max_goal_date - min_goal_date)/86400
+      term = global_end_date.day - global_begin_date.day
 
       sum_amount = goal.map { |x| x.amount }.inject{ |sum, x| sum + x }
       goal_per_day = (sum_amount/term).round(3)
 
     end
 
-    recurrent_incomes = @user.recurrent_payments.where(["recurrent_payments.income = ? AND recurrent_payments.effective_at >= ? AND recurrent_payments.effective_at <= ? ", true, global_begin_date, global_end_date]).sum(:amount)
-    recurrent_spends = @user.recurrent_payments.where(["recurrent_payments.income = ? AND recurrent_payments.effective_at >= ? AND recurrent_payments.effective_at <= ? ", false, global_begin_date, global_end_date]).sum(:amount)
+    recurrent_incomes = @user.recurrent_payments.where(["recurrent_payments.income = ? AND recurrent_payments.effective_at >= ? AND recurrent_payments.effective_at <= ? ", true, global_begin_date.beginning_of_day, global_end_date.end_of_day]).sum(:amount)
+    recurrent_spends = @user.recurrent_payments.where(["recurrent_payments.income = ? AND recurrent_payments.effective_at >= ? AND recurrent_payments.effective_at <= ? ", false, global_begin_date.beginning_of_day, global_end_date.end_of_day]).sum(:amount)
 
-    variable_incomes = @user.variable_payments.where(["variable_payments.income = ? AND variable_payments.created_at >= ? AND variable_payments.created_at <= ? ", true, global_begin_date, global_end_date]).sum(:amount)
-    variable_spends = @user.variable_payments.where(["variable_payments.income = ? AND variable_payments.created_at >= ? AND variable_payments.created_at <= ? ", false, global_begin_date, global_end_date]).sum(:amount)
+    variable_incomes = @user.variable_payments.where(["variable_payments.income = ? AND variable_payments.created_at >= ? AND variable_payments.created_at <= ? ", true, global_begin_date.beginning_of_day, global_end_date.end_of_day]).sum(:amount)
+    variable_spends = @user.variable_payments.where(["variable_payments.income = ? AND variable_payments.created_at >= ? AND variable_payments.created_at <= ? ", false, global_begin_date.beginning_of_day, global_end_date.end_of_day]).sum(:amount)
 
     recurrent_available = recurrent_incomes - recurrent_spends
     variable_available = variable_incomes - variable_spends
 
-    p recurrent_available
-    p variable_available
-
     available = recurrent_available + variable_available
     available = available - amount_spend
-    p available
 
-    term_until_now = Date.today.day - min_goal_date.day
+    term_until_now = day_spend.day - global_begin_date.day
     available_until_now = (available/term_until_now).round(3)
-
-    p available_until_now
-    p goal_per_day
     
     if goal.empty?
       
       result = available > 0
-
-    else
       
-      # sum_amount = goal.map { |x| x.amount }.inject{ |sum, x| sum + x }
+    else
+
       result = available_until_now > goal_per_day
+      
+      # p available_until_now
+      # p goal_per_day
 
     end
 
--- test/models/calculate_test.rb
+++ test/models/calculate_test.rb
@@ -287,90 +287,91 @@ class CalculateTest < ActiveSupport::TestCase
 
   # end
 
-  # test "drop a good answer without any goal" do
+  test "drop a good answer without any goal" do
     
-  #   gabs = User.new
-  #   gabs.email = "gabs@example.com"
-  #   gabs.password = gabs.password_confirmation = "12345678"
-  #   gabs.save!
+    gabs = User.new
+    gabs.email = "gabs@example.com"
+    gabs.password = gabs.password_confirmation = "12345678"
+    gabs.save!
     
-  #   pago = VariablePayment.new
-  #   pago.user = gabs
-  #   pago.amount = 100
-  #   pago.income = true
-  #   pago.created_at = 3.days.ago
-  #   pago.save!
+    pago = VariablePayment.new
+    pago.user = gabs
+    pago.amount = 100
+    pago.income = true
+    pago.created_at = 3.days.ago
+    pago.save!
     
-  #   sushi = VariablePayment.new
-  #   sushi.user = gabs
-  #   sushi.amount = 50
-  #   sushi.income = false
-  #   sushi.created_at = 2.days.ago
-  #   sushi.save!
+    sushi = VariablePayment.new
+    sushi.user = gabs
+    sushi.amount = 50
+    sushi.income = false
+    sushi.created_at = 2.days.ago
+    sushi.save!
     
-  #   chicle = VariablePayment.new
-  #   chicle.user = gabs
-  #   chicle.amount = 30
-  #   chicle.income = false
-  #   chicle.created_at = 1.days.ago
-  #   chicle.save!
+    chicle = VariablePayment.new
+    chicle.user = gabs
+    chicle.amount = 30
+    chicle.income = false
+    chicle.created_at = 1.days.ago
+    chicle.save!
     
-  #   decision = Calculate.new(gabs)
-  #   assert decision.can_spend(10, Date.today) # Yes, you can!
-  #   assert !decision.can_spend(30, Date.today) # No, you can't!
-    
-  # end 
-   
-  test "can I spend some amount wtihout affect my goal?" do
-    
-    # Creacion de un usuario
-    ian = User.new
-    ian.email = "ian@example.com"
-    ian.password = ian.password_confirmation = "12345678"
-    ian.save! # Guardar en el servidor
-    
-    # Ingresos/egresos variables 
-    sueldo = VariablePayment.new
-    sueldo.user = ian
-    sueldo.amount = 200
-    sueldo.income = true
-    sueldo.created_at = 2.days.ago
-    sueldo.save!
-    
-    papas = VariablePayment.new
-    papas.user = ian
-    papas.amount = 10
-    papas.income = false
-    papas.created_at = 2.days.ago
-    papas.save!
-    
-    frutsi = VariablePayment.new
-    frutsi.user = ian
-    frutsi.amount = 5
-    frutsi.income = false
-    frutsi.created_at = 1.days.ago
-    frutsi.save!
-    
-    coca = VariablePayment.new
-    coca.user = ian 
-    coca.amount = 15
-    coca.income = false
-    coca.created_at = Date.today
-    coca.save!
-    
-    # Creacion de una meta
-    tacos = Goal.new
-    tacos.user = ian
-    tacos.amount = 100
-    tacos.start_at = 5.days.ago
-    tacos.end_at = Date.today
-    tacos.save!
-    
-    # Proceso para determinar si se puede gastar o no
-    decision = Calculate.new(ian)
+    decision = Calculate.new(gabs)
     assert decision.can_spend(10, Date.today) # Yes, you can!
-    # assert !decision.can_spend(90, Date.today) # No, you can't!
+    # assert !decision.can_spend(30, Date.today) # No, you can't!
     
-   end
+  end 
+   
+  # test "can I spend some amount wtihout affect my goal?" do
+    
+  #   # Creacion de un usuario
+  #   ian = User.new
+  #   ian.email = "ian@example.com"
+  #   ian.password = ian.password_confirmation = "12345678"
+  #   ian.save! # Guardar en el servidor
+    
+  #   # Ingresos/egresos variables 
+  #   sueldo = VariablePayment.new
+  #   sueldo.user = ian
+  #   sueldo.amount = 200
+  #   sueldo.income = true
+  #   sueldo.created_at = 2.days.ago
+  #   sueldo.save!
+    
+  #   papas = VariablePayment.new
+  #   papas.user = ian
+  #   papas.amount = 10
+  #   papas.income = false
+  #   papas.created_at = 2.days.ago
+  #   papas.save!
+    
+  #   frutsi = VariablePayment.new
+  #   frutsi.user = ian
+  #   frutsi.amount = 5
+  #   frutsi.income = false
+  #   frutsi.created_at = 1.days.ago
+  #   frutsi.save!
+    
+  #   coca = VariablePayment.new
+  #   coca.user = ian 
+  #   coca.amount = 15
+  #   coca.income = false
+  #   coca.created_at = Date.today
+  #   coca.save!
+    
+  #   # Creacion de una meta
+  #   tacos = Goal.new
+  #   tacos.user = ian
+  #   tacos.amount = 100
+  #   tacos.start_at = 5.days.ago
+  #   tacos.end_at = Date.today
+  #   tacos.save!
+    
+  #   # Proceso para determinar si se puede gastar o no
+  #   decision = Calculate.new(ian)
+  #   # $60 is the limit
+  #   assert decision.can_spend(50, Date.today) # Yes, you can!
+  #   assert !decision.can_spend(100, Date.today) # No, you can't!
+    
+  #  end
   
 end
